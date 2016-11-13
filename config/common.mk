4cac3b8214a0346dc8635959e5735b513ddf2296
rpdroky <rpdroky@hotmail.com>
2016-11-13 15:58:17 +0200

    Personalize
    Change-Id: I5b3872d0da25efc0559bbb4a7538ac0448378f4b

--- config/common.mk
+++ config/common.mk
@@ -270,7 +270,7 @@ ifdef CM_BUILDTYPE
     endif
 else
     # If CM_BUILDTYPE is not defined, set to UNOFFICIAL
-    CM_BUILDTYPE := UNOFFICIAL
+    CM_BUILDTYPE := RPDROKY
     CM_EXTRAVERSION :=
 endif
 
